cf02edb66270e86b8a6bdb7db3c15b27f4db84a7
jesus <jesusalvarezxmarts@gmail.com>
2021-03-31 19:28:27 -0400

    Revert "[ADD]submodule paqsa_submodule"
    This reverts commit 8abc563ff8d7c3d1414d2c9d8a5bf6a116c3bdcb.

--- paqsa_submodule/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import res_partner
-from . import models
-from . import pos_config
-from . import stock_picking
--- paqsa_submodule/models/pos_config.py
+++
@@ -1,7 +0,0 @@
-#-*- coding: utf-8 -*-
-from odoo import models, fields, api
-class PosConfig(models.Model):
-    _inherit = "pos.config"
-
-
-
